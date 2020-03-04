14a5c2882bd70ab47543747059d8b8316e4ff671
Tariq <Tariqurahman@gmail.com>
2020-03-04 19:33:05 +0000

    clean up code

--- retrieval.py
+++ retrieval.py
@@ -19,18 +19,7 @@ class Retrieval:
         self.initialize_doc_freq(len(self.documents))
         self.postings = self.index_text_files_rr(self.PATH_TO_DOCS)
         # Boolean retrieval code below
-        #self.index_text_files_br(self.PATH_TO_DOCS)
-
-    def initialize_documents(self, path):
-        """
-        Reads all the documents and stores them in a list to reduce read operations
-        and increase process speed
-        :param path: The path to the document
-        """
-        N = len(sorted(os.listdir(path)))
-        for docID in range(N):
-            s = self.read_file(path, docID)
-            self.documents.append(s)
+        # self.index_text_files_br(self.PATH_TO_DOCS)
 
     @staticmethod
     def read_file(path, docid):
@@ -60,6 +49,136 @@ class Retrieval:
             tokens.remove('')
         # Remove duplicates and return
         return list(dict.fromkeys(tokens))
+
+    @staticmethod
+    def calculate_term_freq(token, doc):
+        """
+        Calculates the number of times the token appears in the document string
+        :param token: The token
+        :param doc: The document
+        :return: (int) term frequency
+        """
+        return doc.count(token)
+
+    def calculate_doc_freq(self, token, N, ):
+        """
+        Calculates the document frequency for a given term (idf)
+        The rarer the term, the higher the score
+        Since there are many re-occuring terms, they are stored in a dict to reduce processing time
+        :param token: The term
+        :param N: Total number of documents
+        :return idf: The document frequency score
+        """
+        # Check if token idf has already been calculated before
+        if token in self.document_frequency:
+            return self.document_frequency[token]
+        else:
+            # calculate document frequency
+            df = 0
+            for doc in self.documents:
+                if token in doc:
+                    df += 1
+            idf = math.log(N / df, 10)
+            # Store idf score to speed up process for high frequency terms
+            self.document_frequency.setdefault(token, idf)
+        return idf
+
+    @staticmethod
+    def calculate_vector_norm(vector):
+        n = 0
+        for key, value in vector.items():
+            n += value ** 2
+        norm = math.sqrt(n)
+        return norm
+
+    def l2normalize(self, docVectors):
+        """
+        Uses the l2norm to normalize the given vectors by dividing each vector by the l2Norm
+        :param docVectors: Document vector to normalize
+        :return: the normalized vector
+        """
+        # find l2norm
+        norm = self.calculate_vector_norm(docVectors)
+        # normalize lengths of vectors
+        for key, value in docVectors.items():
+            docVectors[key] = value / norm
+        return docVectors
+
+    def cosine(self, a, b):
+        """
+        Calculates the cosine of the angle between two given vectors, a and b
+        :param a: Vector a
+        :param b: Vector b
+        """
+        # calculate dot product
+        dot_product = np.dot(list(a.values()), list(b.values()))
+        # calculate magnitude of a and b
+        a_magnitude = self.calculate_vector_norm(a)
+        b_magnitude = self.calculate_vector_norm(b)
+        return dot_product / (a_magnitude * b_magnitude)
+
+    def initialize_documents(self, path):
+        """
+        Reads all the documents and stores them in a list to reduce read operations
+        and increase process speed
+        :param path: The path to the document
+        """
+        N = len(sorted(os.listdir(path)))
+        for docID in range(N):
+            s = self.read_file(path, docID)
+            self.documents.append(s)
+
+    def initialize_doc_freq(self, N):
+        for docID in range(N):
+            s = self.documents[docID]
+            tokens = self.tokenize(s)
+            for t in tokens:
+                self.calculate_doc_freq(t, N)
+
+    def index_text_files_rr(self, path):
+        """
+        Indexes the all the documents and terms into a postings matrix for ranked retrieval
+        :param path: the path to the documents
+        :return postings_matrix: a term document matrix with contents being the tfidf score
+        """
+        N = len(sorted(os.listdir(path)))
+        postings = {}
+        for docID in range(N):
+            s = self.documents[docID]
+            tokens = self.tokenize(s)
+            # Create a template using the keys of the document_frequency dict
+            # This will ensure that all doc vectors have same dimensions
+            document_data = dict.fromkeys(self.document_frequency, 0)
+            for t in tokens:
+                # calculate the relevancy score using tf*idf
+                tf_idf = self.calculate_term_freq(t, s) * self.document_frequency[t]
+                # insert tf_idf score to corresponding terms in dict
+                document_data[t] = tf_idf
+                # Normalize the document scores
+            normalized_data = self.l2normalize(document_data)
+            postings.setdefault(docID, normalized_data)
+        return postings
+
+    def query_rr(self, query, max_results=10):
+        """
+        Query function for ranked retrieval
+        :param query:
+        :param postings:
+        :param max_results:
+        :return:
+        """
+        tokens = self.tokenize(query)
+        # Create dict with zeros
+        tf_idf = dict.fromkeys(self.document_frequency, 0)
+        for t in tokens:
+            # insert idf scores for tokens, tf scores not needed as it will be 1
+            tf_idf[t] = self.document_frequency[t]
+        cosine_scores = {}
+        for key, value in self.postings.items():
+            cosine_scores.setdefault(key, self.cosine(tf_idf, value))
+        # Sort in descending order to get top results
+        result = OrderedDict(sorted(cosine_scores.items(), key=operator.itemgetter(1), reverse=True)[:max_results])
+        return result.keys()
 
     def index_text_files_br(self, path):
         """
@@ -91,125 +210,6 @@ class Retrieval:
         for t in tokens:
             result = postings[t] if result is None else result & postings[t]
         return result
-
-    def query_rr(self, query, max_results=10):
-        """
-        Query function for ranked retrieval
-        :param query:
-        :param postings:
-        :param max_results:
-        :return:
-        """
-        tokens = self.tokenize(query)
-        # Create dict with zeros
-        tf_idf = dict.fromkeys(self.document_frequency, 0)
-        for t in tokens:
-            # insert idf scores for tokens, tf scores not needed as it will be 1
-            tf_idf[t] = self.document_frequency[t]
-        cosine_scores = {}
-        for key, value in self.postings.items():
-            cosine_scores.setdefault(key, self.cosine(tf_idf, value))
-        # Sort in descending order to get top results
-        result = OrderedDict(sorted(cosine_scores.items(), key=operator.itemgetter(1), reverse=True)[:max_results])
-        return result.keys()
-
-    def index_text_files_rr(self, path):
-        """
-        Indexes the all the documents and terms into a postings matrix for ranked retrieval
-        :param path: the path to the documents
-        :return postings_matrix: a term document matrix with contents being the tfidf score
-        """
-        N = len(sorted(os.listdir(path)))
-        postings = {}
-        for docID in range(N):
-            s = self.documents[docID]
-            tokens = self.tokenize(s)
-            # Create a template using the keys of the document_frequency dict
-            # This will ensure that all doc vectors have same dimensions
-            row_data = dict.fromkeys(self.document_frequency, 0)
-            for t in tokens:
-                # calculate the relevancy score using tf*idf
-                tf_idf = self.calculate_term_freq(t, s) * self.document_frequency[t]
-                # insert tf_idf score to corresponding terms in dict
-                row_data[t] = tf_idf
-                # Normalize the document scores
-            normalized_data = self.l2normalize(row_data)
-            postings.setdefault(docID, normalized_data)
-        return postings
-
-    @staticmethod
-    def calculate_term_freq(token, doc):
-        """
-        Calculates the number of times the token appears in the document string
-        :param token: The token
-        :param doc: The document
-        :return: (int) term frequency
-        """
-        return doc.count(token)
-
-    def cosine(self, a, b):
-        """
-        Calculates the cosine of the angle between two given vectors, a and b
-        :param a: Vector a
-        :param b: Vector b
-        """
-        # calculate dot product
-        dot_product = np.dot(list(a.values()), list(b.values()))
-        # calculate magnitude of a and b
-        a_magnitude = self.calculate_vector_norm(a)
-        b_magnitude = self.calculate_vector_norm(b)
-        return dot_product / (a_magnitude * b_magnitude)
-
-    def calculate_doc_freq(self, token, N, ):
-        """
-        Calculates the document frequency for a given term (idf)
-        The rarer the term, the higher the score
-        Since there are many re-occuring terms, they are stored in a dict to reduce processing time
-        :param token: The term
-        :param N: Total number of documents
-        :return idf: The document frequency score
-        """
-        # Check if token idf has already been calculated before
-        if token in self.document_frequency:
-            return self.document_frequency[token]
-        else:
-            # calculate document frequency
-            df = 0
-            for doc in self.documents:
-                if token in doc:
-                    df += 1
-            idf = math.log(N / df, 10)
-            # Store idf score to speed up process for high frequency terms
-            self.document_frequency.setdefault(token, idf)
-        return idf
-
-    def l2normalize(self, docVectors):
-        """
-        Uses the l2norm to normalize the given vectors by dividing each vector by the l2Norm
-        :param docVectors: Document vector to normalize
-        :return: the normalized vector
-        """
-        # find l2norm
-        norm = self.calculate_vector_norm(docVectors)
-        # normalize lengths of vectors
-        for key, value in docVectors.items():
-            docVectors[key] = value / norm
-        return docVectors
-
-    @staticmethod
-    def calculate_vector_norm(vector):
-        n = 0
-        for key, value in vector.items():
-            n += value ** 2
-        norm = math.sqrt(n)
-        return norm
-
-    def initialize_doc_freq(self, N):
-        for docID in range(N):
-            s = self.documents[docID]
-            tokens = self.tokenize(s)
-            for t in tokens:
-                self.calculate_doc_freq(t, N)
 
 
 retrieval = Retrieval()
